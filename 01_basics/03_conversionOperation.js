87f80d5a5234deacd2005ee42edbfe80e5d0193c
vankishgupta <vankishgupta@gmail.com>
2025-07-28 13:44:56 +0000

    conversion of dataypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score=33
+console.log(typeof score)
+let valueInNumber=Number(score);
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+console.log(typeof score)
+let sc =true;
+console.log(sc)
+let n1=Number(sc);
+console.log(n1)
+
+
+// theory 
+// when we convert string to Number 
+//"33"-> 33
+// "33abc"=>NaN
+// true=>1;false=>0
+
+let isflag=1
+let boolisflag=Boolean(isflag)
+console.log(boolisflag)
+console.log(typeof boolisflag)
+
+// theory 
+// when we convert number to boolean
+// 1=>true, 0=>false
+// ""=>false
+// "hitesh"=>true
