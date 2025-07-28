cc1eea46551beb79ff3490ae46fe01d2027cedbd
vankishgupta <vankishgupta@gmail.com>
2025-07-28 14:37:51 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,13 +1,13 @@
 let score=33
-console.log(typeof score)
+//console.log(typeof score)
 let valueInNumber=Number(score);
-console.log(valueInNumber)
-console.log(typeof valueInNumber)
-console.log(typeof score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(typeof score)
 let sc =true;
-console.log(sc)
+//console.log(sc)
 let n1=Number(sc);
-console.log(n1)
+//console.log(n1)
 
 
 // theory 
@@ -18,11 +18,31 @@ console.log(n1)
 
 let isflag=1
 let boolisflag=Boolean(isflag)
-console.log(boolisflag)
-console.log(typeof boolisflag)
+// console.log(boolisflag)
+// console.log(typeof boolisflag)
 
 // theory 
 // when we convert number to boolean
 // 1=>true, 0=>false
 // ""=>false
 // "hitesh"=>true
+
+
+
+
+// ****** OPERATIONS *******
+let value = 3
+let negvalue= -value
+//console.log(negvalue)
+// NOTE:for a raise to power b use a**b
+
+// to string can also be combined
+let s1="vankish"
+let s2=" gupta"
+let s3=s1+s2
+console.log(s3)
+
+console.log("1" + 2 )// 12
+console.log(1 + "2")//12
+console.log("1" +2 +2)//122
+console.log(1+2+"2")//32
