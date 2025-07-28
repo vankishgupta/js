e3dc8ddf603fea1af2c63387b6d7a3a95b73961c
vankishgupta <vankishgupta@gmail.com>
2025-07-28 14:48:10 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,5 @@
+// ===
+console.log("2"===2)//strict check
+console.log("2"==2)
+// final result agr two same datatype ko compare kar rahe ho toh simple comparison
+//use karo otherwise strict comparison use karo 
