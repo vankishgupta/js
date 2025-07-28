3b84b56cc6dfb747482767cc156b04d3560a07f9
vankishgupta <vankishgupta@gmail.com>
2025-07-28 13:11:47 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";
+//means it treat all JS CODE as newer version
+
+// alert(3+3)// we are using nodejs,not browser
+console.log(3+3) // code readbility should be high
+console.log("hitesh")
+
+let name="hitesh"
+let age =18
+let isloggedin=false
+
+
+//  number => 2 to power 53
+// big int (not needed for us ,used in quant and trading )
+// string =>""
+// boolean =>true/false
+// null=>standalone value
+// undefined =>
+// symbol =>unique
+
+
+// object
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isloggedin)
+
+console.log(typeof undefined);// undefined 
+console.log(typeof null); // object
---
+++ 01_basics/02_variable.js
@@ -0,0 +1,12 @@
+const accountID=14453
+let accountEmail="hitesh@google.com"
+var accountpassword="12345"
+accountcity="jaipur" // works but no preferred 
+// accountID=2 // not allowed 
+let accountstate
+ 
+acccountEmail="hc@hc.com"
+accountpassword="21212121"
+accountcity="bengaluru"
+console.log(accountID)
+console.table([accountID,accountEmail,accountpassword,accountcity,accountstate])
