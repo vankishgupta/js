fec7cdea693ef79e7aa66dab9bc42a77aa29c937
vankishgupta <vankishgupta@gmail.com>
2025-08-04 06:19:02 +0000

    for loop

---
+++ 05_loops/one.js
@@ -0,0 +1,16 @@
+// for loop
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+    
+}
+// NESTED LOOP
+for(let i=0;i<=10;i++){
+    console.log(`outer loop value:${i}`);
+    for(let j=0;j<=10;j++){
+        console.log(`Inner loop value ${j} and inner loop ${i}`);
+    }
+}
+
+
+// break and continue
