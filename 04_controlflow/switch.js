62991e5ee11250db525d6cd09b6f90a30d4e31ab
vankishgupta <vankishgupta@gmail.com>
2025-08-04 05:03:22 +0000

    switch case statement

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,31 @@
+//  SWITCH CASE 
+// BASIC SYNTAX FOR SWITCH CASE
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//example 
+const month=3;
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    
+    default:
+        console.log("default case");
+        break;
+}
