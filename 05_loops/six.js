bee7d30376521bb65e7f07d1dc79f4a80ef6f41a
vankishgupta <vankishgupta@gmail.com>
2025-08-04 10:03:04 +0000

    filter loop

---
+++ 05_loops/six.js
@@ -0,0 +1,14 @@
+// for each loop value ko print kar sakta ha bass 
+// fliter values ko return bhi kardega 
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+const newnums=myNums.filter((num)=>num>4)
+console.log(newnums)
+// or 
+// const newnums=myNums.filter((num)=>{return num>4})
+// both are valid 
+// agr scope open kiya ha toh use return keyword to return anthing 
+// else if no scope then no need of return keyword
+
+const chai=()=>{
+
+}
