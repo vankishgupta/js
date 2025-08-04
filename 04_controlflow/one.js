12eebef7b8bb01b0ce745d4d58f1f873ffdf6711
vankishgupta <vankishgupta@gmail.com>
2025-08-04 04:53:53 +0000

    if -else laddedr

---
+++ 04_controlflow/one.js
@@ -0,0 +1,26 @@
+// if
+
+// if(condition){
+//     //condition has to be true to execute
+// }
+// example 
+const isuserloggedin=true;
+if(isuserloggedin){
+    // just an example
+}
+ //
+ // <,>,<=,>=,==,!=,===
+//  if(2=="2")console.log("executed");// run 
+//  if(2==="2")console.log("executed");// do not run
+
+ const score=200
+//  if(score>100){
+//     const power="fly"
+//     console.log(`user power:${power}`);
+//  }
+
+const userLoggedin=true
+const debitcard=true;
+if(userLoggedin && debitcard){
+    //console.log("allow to buy coourse");
+}
