ff44acc1bc3c7a97a1287b6545836f161100c29d
vankishgupta <vankishgupta@gmail.com>
2025-08-04 06:28:01 +0000

    while and do while loops

---
+++ 05_loops/two.js
@@ -0,0 +1,25 @@
+// while loop 
+ 
+// while(condition){
+//     execution,
+// }
+let i=0;
+while(i<10){
+    console.log(`value of index is ${i}`)
+    i+=2;
+}
+
+let myarr=["flash","batman","siperman"];
+let j=0;
+while(j<myarr.length){
+    console.log(myarr[j]);
+    j++;
+}
+
+
+// DO WHILE LOOP //
+let score=11;
+do {
+    console.log(`score is${score}`);
+    score++;
+} while (score<=10);// ek bar cha jayega 
